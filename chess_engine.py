d432bd431c025883c252ba48281833ec8a091f45
difbit <fruktoosijee@gmail.com>
2020-11-15 19:24:40 +0200

    Refactoring and better board view. Also updated code for Python 3. There is an issue with first pawn move, because it can jump over a piece.

--- chess_engine.py
+++ chess_engine.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
 import random
@@ -9,7 +9,7 @@ from itertools import chain
 import itertools
 import pickle
 
-tic = time.clock()
+tic = time.process_time()
 
 CHECKLIST_ONE = [24, 35, 108, 119]
 CHECKLIST_TWO = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34,
@@ -19,36 +19,36 @@ CHECKLIST_TWO = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34,
 
 """Use these two boards to play around"""
 
-#The board representation
+# The board representation
 posiii = [
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,                  #0-11
-    0, 0, 0, 0, 0, 0 ,0, 0, 0, 0, 0, 0,                  #12-23
-    0, 0, 'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r', 0, 0,  #24-35     #26-33
-    0, 0, 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 0, 0,  #36-47     #38-45
-    0, 0, '-', '-', '-', '-', '-', '-', '-', '-', 0, 0,  #48-59     #50-57
-    0, 0, '-', '-', '-', '-', '-', '-', '-', '-', 0, 0,  #60-71     #62-69
-    0, 0, '-', '-', '-', '-', '-', '-', '-', '-', 0, 0,  #72-83     #74-81
-    0, 0, '-', '-', '-', '-', '-', '-', '-', '-', 0, 0,  #84-95     #86-93
-    0, 0, 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 0, 0,  #96-107    #98-105
-    0, 0, 'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R', 0, 0,  #108-119   #110-117
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,                  #120-131
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0                   #132-143
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,                  # 0-11
+    0, 0, 0, 0, 0, 0 ,0, 0, 0, 0, 0, 0,                  # 12-23
+    0, 0, 'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r', 0, 0,  # 24-35     # 26-33
+    0, 0, 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 0, 0,  # 36-47     # 38-45
+    0, 0, '-', '-', '-', '-', '-', '-', '-', '-', 0, 0,  # 48-59     # 50-57
+    0, 0, '-', '-', '-', '-', '-', '-', '-', '-', 0, 0,  # 60-71     # 62-69
+    0, 0, '-', '-', '-', '-', '-', '-', '-', '-', 0, 0,  # 72-83     # 74-81
+    0, 0, '-', '-', '-', '-', '-', '-', '-', '-', 0, 0,  # 84-95     # 86-93
+    0, 0, 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 0, 0,  # 96-107    # 98-105
+    0, 0, 'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R', 0, 0,  # 108-119   # 110-117
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,                  # 120-131
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0                   # 132-143
     ]
 
-#The board for testing moves and functions of different pieces
+# The board for testing moves and functions of different pieces
 posi = [
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,                  #0-11
-    0, 0, 0, 0, 0, 0 ,0, 0, 0, 0, 0, 0,                  #12-23
-    0, 0, 'r', '-', '-', '-', 'k', '-', '-', 'r', 0, 0,  #24-35     #26-33
-    0, 0, '-', '-', '-', '-', '-', '-', '-', '-', 0, 0,  #36-47     #38-45
-    0, 0, '-', '-', '-', '-', '-', '-', '-', '-', 0, 0,  #48-59     #50-57
-    0, 0, '-', '-', '-', '-', '-', '-', '-', '-', 0, 0,  #60-71     #62-69
-    0, 0, '-', '-', '-', '-', '-', '-', '-', '-', 0, 0,  #72-83     #74-81
-    0, 0, '-', '-', '-', '-', '-', '-', '-', '-', 0, 0,  #84-95     #86-93
-    0, 0, '-', '-', '-', '-', '-', '-', '-', '-', 0, 0,  #96-107    #98-105
-    0, 0, 'R', '-', '-', '-', 'K', '-', '-', 'R', 0, 0,  #108-119   #110-117
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,                  #120-131
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0                   #132-143
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,                  # 0-11
+    0, 0, 0, 0, 0, 0 ,0, 0, 0, 0, 0, 0,                  # 12-23
+    0, 0, 'r', '-', '-', '-', 'k', '-', '-', 'r', 0, 0,  # 24-35     # 26-33
+    0, 0, '-', '-', '-', '-', '-', '-', '-', '-', 0, 0,  # 36-47     # 38-45
+    0, 0, '-', '-', '-', '-', '-', '-', '-', '-', 0, 0,  # 48-59     # 50-57
+    0, 0, '-', '-', '-', '-', '-', '-', '-', '-', 0, 0,  # 60-71     # 62-69
+    0, 0, '-', '-', '-', '-', '-', '-', '-', '-', 0, 0,  # 72-83     # 74-81
+    0, 0, '-', '-', '-', '-', '-', '-', '-', '-', 0, 0,  # 84-95     # 86-93
+    0, 0, '-', '-', '-', '-', '-', '-', '-', '-', 0, 0,  # 96-107    # 98-105
+    0, 0, 'R', '-', '-', '-', 'K', '-', '-', 'R', 0, 0,  # 108-119   # 110-117
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,                  # 120-131
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0                   # 132-143
     ]
 
 player_posi = {
@@ -62,33 +62,45 @@ player_posi = {
     'a1': 110, 'b1': 111, 'c1': 112, 'd1': 113, 'e1': 114, 'f1': 115, 'g1': 116, 'h1': 117,
 }
 
-#Used for printing the board
-def board_view(list):
-    view = [list[i:i + 12] for i in xrange(0, 11, 12)]
+
+# Used for printing the board
+def board_list_view(list):
+    view = [list[i:i + 12] for i in range(0, 11, 12)]
     o = '\n'.join(map(str, view))
-    view_2 = [list[i:i + 12] for i in xrange(12, 23, 12)]
+    view_2 = [list[i:i + 12] for i in range(12, 23, 12)]
     o_2 = '\n'.join(map(str, view_2))
-    view_3 = [list[i:i + 12] for i in xrange(24, 117, 12)]
+    view_3 = [list[i:i + 12] for i in range(24, 117, 12)]
     o_3 = '\n'.join(map(str, view_3))
-    view_4 = [list[i:i + 12] for i in xrange(120, 131, 12)]
+    view_4 = [list[i:i + 12] for i in range(120, 131, 12)]
     o_4 = '\n'.join(map(str, view_4))
-    view_5 = [list[i:i + 12] for i in xrange(132, 143, 12)]
+    view_5 = [list[i:i + 12] for i in range(132, 143, 12)]
     o_5 = '\n'.join(map(str, view_5))
     good_board = '\t%s\n\t%s\n%s\n\t%s\n\t%s\n' % (o, o_2, o_3, o_4, o_5)
     return good_board
 
-#Illegal positions for pieces
+
+def board_view(list):
+    view_string = ''
+    for col in range(1, 13):
+        view = [list[i:i + 12] for i in range(12 * (col - 1), (col * 12) - 1, 12)]
+        view_string += '%s%s' % ('\n\t', ' '.join(str(v) for v in view[0]))
+    result = view_string.replace('0', '|')
+    return result
+
+
+# Illegal positions for pieces
 ZEROS = [i for i, x in enumerate(posi) if x == 0]
 
 X, Y = 1, 12
 moves = {
-"rook" : [X, Y, -X, -Y],
-"knight" : [Y+Y-X, Y+Y+X, X+X+Y, X+X-Y, -Y-Y+X, -Y-Y-X, -X-X-Y, -X-X+Y],
-"bishop" : [X+Y, X-Y, -X+Y, -X-Y],
-"queen" : [X, Y, -X, -Y, X+Y, X-Y, -X+Y, -X-Y],
-"king" : [X, Y, -X, -Y, X+Y, X-Y, -X+Y, -X-Y, -X-X, X+X], # Last two moves are for castling
-"pawn" : [-Y, -Y-Y, -Y+X, -Y-X]
+    "rook": [X, Y, -X, -Y],
+    "knight": [Y+Y-X, Y+Y+X, X+X+Y, X+X-Y, -Y-Y+X, -Y-Y-X, -X-X-Y, -X-X+Y],
+    "bishop": [X+Y, X-Y, -X+Y, -X-Y],
+    "queen": [X, Y, -X, -Y, X+Y, X-Y, -X+Y, -X-Y],
+    "king": [X, Y, -X, -Y, X+Y, X-Y, -X+Y, -X-Y, -X-X, X+X],  # Last two moves are for castling
+    "pawn": [-Y, -Y-Y, -Y+X, -Y-X]
 }
+
 
 # The function for rotating the board; used when a move is made
 # and it is other player's move.
@@ -113,27 +125,32 @@ _pieces = {
     "knight": 'N', "queen": 'Q', "bishop": 'B'
     }
 
+
 def lower_case(d):
-    new = dict((k.lower(), v.lower()) for k,v in d.iteritems())
+    new = dict((k.lower(), v.lower()) for k,v in d.items())
     return new
+
 
 def upper_case(d):
-    new = dict((k, v.upper()) for k,v in d.iteritems())
+    new = dict((k, v.upper()) for k,v in d.items())
     return new
+
 
 def upper_keys(d):
-    new = dict((k.upper(), v) for k,v in d.iteritems())
+    new = dict((k.upper(), v) for k,v in d.items())
     return new
 
-#Maybe will be used later
+
+# Maybe will be used later
 def low(d):
-    new = dict((k.lower(), v) for k,v in d.iteritems())
+    new = dict((k.lower(), v) for k,v in d.items())
     return new
 
 
 def save_list(target_list):
     with open('target_list', 'wb') as f:
         pickle.dump(target_list, f)
+
 
 def open_list(target_list):
     with open('target_list', 'rb') as f:
@@ -181,6 +198,7 @@ class board_object(object):
         self.posi = posi
         self.posi = list(self.posi)
 
+
 def chess_engine(): #This will be used for the evaluation function
 
     BOARD_LIST = []
@@ -196,7 +214,7 @@ def chess_engine(): #This will be used for the evaluation function
 
     while depth < LIMIT:
         a_game.move_gen(posi)
-        #print board_view(posi)
+        #print(board_view(posi))
         #if first_save:
         #    this_board = board_object(posi)
             #BOARD_LIST[1] = board_object(posi)
@@ -219,8 +237,8 @@ def chess_engine(): #This will be used for the evaluation function
         depth += 1
 
     for i in range(0, LIMIT):
-        print EVALUATIONS[i]
-    print board_view(BOARD_LIST[LIMIT].posi)
+        print(EVALUATIONS[i])
+    print(board_view(BOARD_LIST[LIMIT].posi))
     #Boards could be keys and evaluations values in dictionary
 
 def capture_piece():
@@ -291,7 +309,7 @@ def get_piece_moves(posi, white_to_move, _pieces):
 
                     for go in range(1, rang):
                         if mod_posi[enum_piece + move * go] in \
-                                (ZEROS + _pieces.values()):
+                                (ZEROS + list(_pieces.values())):
                             break
                         elif mod_posi[enum_piece + move * go] != '-':
                             ate_piece = True
@@ -318,8 +336,10 @@ def get_piece_moves(posi, white_to_move, _pieces):
                             break
     return positions, posi
 
+
 def bool_negation(bool):
     return not bool
+
 
 def search_checks(orig_fetched_moves, white_to_move):
     white_to_move = bool_negation(white_to_move)
@@ -341,13 +361,14 @@ def search_checks(orig_fetched_moves, white_to_move):
     white_to_move = bool_negation(white_to_move)
     return orig_fetched_moves, white_to_move
 
+
 def play_move(position, searched_moves):
     while True:
         pos = list(position)
-        print "Select a piece from: \n%s" % sorted(player_posi.keys())
-        start = raw_input()
-        print "Select a destination square from: \n%s" % sorted(player_posi.keys())
-        dest = raw_input()
+        print("Select a piece from: \n%s" % sorted(player_posi.keys()))
+        start = input()
+        print("Select a destination square from: \n%s" % sorted(player_posi.keys()))
+        dest = input()
         start_posi = player_posi[start]
         dest_posi = player_posi[dest]
         if pos[dest_posi] != '-':
@@ -359,30 +380,31 @@ def play_move(position, searched_moves):
                 not player_posi.get(start) and player_posi.get(dest)
                 or start == dest
             ):
-            print "Invalid square"
+            print("Invalid square")
             continue
         else:
             pos[start_posi], pos[dest_posi] = square, pos[start_posi]
-            print "DEST", posi[player_posi[dest]]
+            print("DEST", posi[player_posi[dest]])
             if pos in searched_moves:
                 return pos
             else:
                 continue
 
 
-print board_view(posiii)
+position = list(posiii)
+print(board_view(position))
 white_to_move = True
 
 # Change starting position here
-fetched_moves = get_piece_moves(posiii, white_to_move, _pieces)
+fetched_moves = get_piece_moves(position, white_to_move, _pieces)
 orig_fetched_moves = list(fetched_moves)
 searched_moves = search_checks(orig_fetched_moves, white_to_move)
-#print "SEARCHED MOVES", searched_moves
+#print("SEARCHED MOVES", searched_moves)
 
-random_posi = play_move(posiii, searched_moves[0][0])
+random_posi = play_move(position, searched_moves[0][0])
 
 #random_posi = random.choice(searched_moves[0][0])
-print board_view(random_posi)
+print(board_view(random_posi))
 if white_to_move:
     if random_posi[110] != 'R':
         king_rook_move['K']['ROOK_Q'] = True
@@ -398,7 +420,7 @@ else:
     if random_posi[114] != 'k':
         king_rook_move['k']['KING'] = True
 
-#print "WHITE TO MOVE", white_to_move
+#print("WHITE TO MOVE", white_to_move)
 
 while True:
     white_to_move = bool_negation(white_to_move)
@@ -407,7 +429,7 @@ while True:
     orig_fetched_moves = list(fetched_moves)
     searched_moves = search_checks(orig_fetched_moves, white_to_move)
     if searched_moves[0][0] == []:
-        print "It is checkmate or stalemate"
+        print("It is checkmate or stalemate")
         exit()
 
     if white_to_move:
@@ -429,15 +451,15 @@ while True:
             king_rook_move['k']['ROOK_K'] = True
         if random_posi[114] != 'k':
             king_rook_move['k']['KING'] = True
-    #print king_rook_move
-    #print random_posi[114]
+    #print(king_rook_move)
+    #print(random_posi[114])
     if not white_to_move:
-        print board_view(rotate(list(random_posi)))
+        print(board_view(rotate(list(random_posi))))
     else:
-        print board_view(random_posi)
-    #print "WHITE TO MOVE", white_to_move
+        print(board_view(random_posi))
+    #print("WHITE TO MOVE", white_to_move)
     time.sleep(4.7)
 ###############
 
-toc = time.clock()
-print "time spent:", toc - tic
+toc = time.process_time()
+print("time spent:", toc - tic)
